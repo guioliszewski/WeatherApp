dd3fd27f4e72378988131d4b3c43654be6c2ac44
guioliszewski <guilhermeoliszewski@outlook.com>
2025-05-04 00:15:27 -0300

    created types for forecastData and weatherData

--- weatherApp/src/App.tsx
+++ weatherApp/src/App.tsx
@@ -1,4 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+type weatherData = {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+  feelsLike: number;
+  wind: {
+    speed: number;
+  };
+};
+
+type forecastData = {
+  list: {
+    dt: number;
+    main: {
+      temp: number;
+    };
+    weather: {
+      icon: string;
+    }[];
+  }[];
+};
 
 function App() {
   return (
